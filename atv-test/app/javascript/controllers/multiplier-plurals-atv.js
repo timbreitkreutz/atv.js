124fb0fbd9e11a307772a31676cde021e799d218
Timothy Breitkreutz <tim@sbrew.com>
2024-11-17 13:03:17 -0500

    missed a test file

---
+++ atv-test/app/javascript/controllers/multiplier-plurals-atv.js
@@ -0,0 +1,25 @@
+
+function connect(targets) {
+  return {
+    multiply: function() {
+      let product = 1;
+      targets.aFactors.forEach((factor) => {
+        if (factor.value) {
+          product *= factor.value;
+        } else {
+          product *= factor.innerText;
+        }
+      });
+      if (targets.product) {
+        targets.product.innerText = product;
+      }
+      return product;
+    },
+    blur: function(actor) {
+      targets.extra.innerText = "DONE";
+      return true;
+    }
+  };
+}
+
+export { connect };
